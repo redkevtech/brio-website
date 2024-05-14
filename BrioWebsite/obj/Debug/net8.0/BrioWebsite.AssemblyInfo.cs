48a75dcec11af6c51ef3779472c190f13b3396b8
Kevin <117398066+roig-kevinjentes@users.noreply.github.com>
2024-05-14 10:37:04 -0400

    updates

--- BrioWebsite/obj/Debug/net8.0/BrioWebsite.AssemblyInfo.cs
+++ BrioWebsite/obj/Debug/net8.0/BrioWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174cded120e43929010f2cab98154de8395d656f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693bfbf81fb2946fa816080aef986e2c297dc7b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
